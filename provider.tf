8b06e95273d1b9681879a69ce64c31a557176c21
Mohzes <85204206+momagdyy97@users.noreply.github.com>
2024-09-05 08:36:59 +0300

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,12 @@
+# provider.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+provider "aws" {
+  region = "me-central-1"
+}
